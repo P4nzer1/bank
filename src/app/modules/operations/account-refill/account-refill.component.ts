e5987727287377052d3ba1c971d836bfb3a1752e
Nikita Smirov <nikitka-smirnov-2013@mail.ru>
2024-10-18 00:44:31 +0300

    200refil

--- src/app/modules/operations/account-refill/account-refill.component.ts
+++ src/app/modules/operations/account-refill/account-refill.component.ts
@@ -22,7 +22,7 @@ export class AccountRefillComponent implements OnInit {
   ) {
     this.refillForm = this.fb.group({
       accountNumber: ['', Validators.required],
-      amount: ['', [Validators.required, Validators.min(1)]]
+      amount: ['', [Validators.required, Validators.min(0), Validators.max(1000000000)]]
     });
   }
 
@@ -44,26 +44,83 @@ export class AccountRefillComponent implements OnInit {
   
     this.loading = true; // Включаем флаг загрузки
   
-    this.accountOperationService.refillAccount(accountNumber, amount).subscribe({
+    // Запуск операции пополнения счета
+    this.accountOperationService.startOperation('AccountRefill').subscribe({
       next: (response) => {
-        console.log('Счет успешно пополнен', response);
-        
-        // Обновляем данные о счетах пользователя
-        this.productService.getAccounts().subscribe({
-          next: (updatedAccounts) => {
-            console.log('Обновленные счета:', updatedAccounts);
-            this.loading = false; // Отключаем флаг загрузки
-          },
-          error: (err) => {
-            console.error('Ошибка при обновлении счетов', err);
-            this.loading = false;
-          }
-        });
+        console.log('Операция пополнения счета успешно запущена', response);
+        const requestId = response.requestId;
+        if (requestId) {
+          // Передача данных на втором шаге
+          this.handleStepParams(response.stepParams, requestId, accountNumber, amount);
+        } else {
+          console.error('Request ID не получен');
+          this.loading = false;
+        }
       },
       error: (err) => {
-        console.error('Ошибка при пополнении счета', err);
-        this.errorMessage = 'Ошибка при пополнении счета';
+        console.error('Ошибка при запуске операции:', err);
+        this.errorMessage = 'Ошибка при запуске операции';
+        this.loading = false;
+      }
+    });
+  }
+  
+  handleStepParams(stepParams: any[], requestId: string, accountNumber: string, amount: number): void {
+    const formData = this.collectStepFormData(stepParams, accountNumber, amount);
+    this.proceedOperation(requestId, formData);
+  }
+  
+  collectStepFormData(stepParams: any[], accountNumber: string, amount: number): { name: string, value: any }[] {
+    const formData: { name: string, value: any }[] = [];
+  
+    stepParams.forEach((param: any) => {
+      if (param.name === 'Счёт пополнения') {
+        formData.push({
+          name: param.name,
+          value: String(accountNumber) // Преобразование accountNumber в строку
+        });
+      } else if (param.name === 'Сумма пополнения') {
+        formData.push({
+          name: param.name,
+          value: Number(amount) // Преобразование amount в число
+        });
+      }
+    });
+  
+    return formData;
+  }
+  
+  
+
+  proceedOperation(requestId: string, stepData: { name: string, value: any }[]): void {
+    this.accountOperationService.proceedOperation(requestId, stepData).subscribe({
+      next: response => {
+        console.log('Операция успешно обработана', response);
+        // Проверка, есть ли шаги для следующей операции
+        if (response.stepParams) {
+          this.handleStepParams(response.stepParams, requestId, stepData[0].value, stepData[1].value);
+        } else {
+          console.log('Операция завершена');
+          this.updateAccounts(); // Обновляем данные счетов после завершения операции
+        }
+      },
+      error: err => {
+        console.error('Ошибка при выполнении следующего шага операции:', err);
+        this.loading = false;
+      }
+    });
+  }
+
+  // Обновление счетов после завершения операции
+  updateAccounts(): void {
+    this.productService.getAccounts().subscribe({
+      next: (updatedAccounts) => {
+        console.log('Обновленные счета:', updatedAccounts);
         this.loading = false; // Отключаем флаг загрузки
+      },
+      error: (err) => {
+        console.error('Ошибка при обновлении счетов', err);
+        this.loading = false;
       }
     });
   }
